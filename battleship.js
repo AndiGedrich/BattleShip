eb0db6e5e8f3f698f8781a5dd252d924e28d98cc
Andrea Gedrich <andrea.d.amaro@gmail.com>
2015-12-01 09:50:39 -0800

    and again

--- battleship.js
+++ battleship.js
@@ -1,5 +1,27 @@
 $(function(){
+  console.log("main js loaded.");
 
-console.log("main js loaded.");
+  var airCraftCarrier = $('#myAircraftCarrier'); length: 5;
+  var battleShip= $('#myBattleship'); length: 4;
+  var Cruiser= $('#myCruiser'); length: 3;
+  var PatrolBoat= $('#myPatrolBoat'); length: 2;
+  var submarine= $('#mySubmarine'); length: 3;
 
-)}
+  var assignShipArray = function (){
+
+  }
+  airCraftCarrier.addEventListener("clicked",
+
+})
+
+
+
+
+
+
+
+
+//   $('#myShipLayout').click(function(){
+//     $('#viewToggle').toggle();
+//   }
+
