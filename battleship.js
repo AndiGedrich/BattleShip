a31005f410702b097f95bbf3040eeb311944862c
Andrea Gedrich <andrea.d.amaro@gmail.com>
2015-12-02 14:14:45 -0800

    made more progress on game play

--- battleship.js
+++ battleship.js
@@ -5,13 +5,13 @@ $(function(){
 // dummy board
 
 //shot counter
-document.getElementById("shotCounter").value = "25";
+
 
 var ships= {
   AircraftCarrier:["C3", "C4", "C5","C6","C7"],
   Battleship:["E2", "F2", "G2","H2"],
   Submarine:["G5", "G6", "G7"],
-  Cruiser :["A9", "B9", "C9"],
+  Cruiser:["A9", "B9", "C9"],
   PatrolBoat:["E9", "E10"],
 }
 
@@ -20,6 +20,7 @@ console.log (ships);
 console.log (keys);
 
 $('.Player1Board').click(function(){
+  document.querySelector("h3").innerHTML= " ";
   document.getElementById("shotCounter").value-=1;
   console.log ("shot value:" + this.id);
   var shot= this.id;
@@ -33,21 +34,53 @@ $('.Player1Board').click(function(){
       if(shot === button){
         hit = true;
         document.querySelector("h3").innerHTML= "HIT!";
-        document.getElementById(shot).value="X";
-        document.getElementById(shot).style.background="red";
-        document.getElementById(shot).disabled="disabled";
+        var att= document.createAttribute("class");
+          att.value = "hitShipCell";
+          document.getElementById(shot).className += "hitShipCell";
+          document.getElementById(shot).value="X";
+          document.getElementById(shot).disabled="disabled";
         }
       }
     }
+  if (!hit) {
+      document.querySelector("h3").innerHTML= "MISS";
+      document.getElementById(shot).style.background="white";
+      document.getElementById(shot).disabled="disabled";
+  }
 
-    if (!hit) {
-        document.querySelector("h3").innerHTML= "MISS";
-        document.getElementById(shot).style.background="white";
-        document.getElementById(shot).disabled="disabled";
-        }
-    })
+  for (i in keys){
+    var key = keys [i];
+    // do something
+    hitCount = 0;
+    for (j in ships[key]){
+      if ($('#' + ships[key][j]).hasClass('Player1BoardhitShipCell')) {
+        hitCount += 1
+      }
+    }
+    if (ships[key].length === hitCount) {
+        console.log('SUNK!');
+        sunkShipId = key;
+        console.log ("SunkShip:" + sunkShipId);
+        document.querySelector("h3").innerHTML= key + " IS SUNK!";
+        document.getElementById(sunkShipId).src = key + "_sunk.jpg";
+        var attShip= document.createAttribute("class");
+          attShip.value = "Sunk";
+          document.getElementById(sunkShipId).className += "Sunk";
+        var checkWinner = function (){
+          if ($('#AircraftCarrier').hasClass('Sunk') &&
+              $('#Battleship').hasClass('Sunk') &&
+              $('#Submarine').hasClass('Sunk') &&
+              $('#Cruiser').hasClass('Sunk') &&
+              $('#PatrolBoat').hasClass('Sunk')) {
+                document.querySelector("h3").innerHTML= "PLAY 1 WINS";
+                location.reload();
+              }
+          }
+    }
+   }
+
+  })
 });
-
 
 
 //   var CONST = {};
